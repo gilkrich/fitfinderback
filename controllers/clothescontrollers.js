21d9c2efd71fdaad89eed5297e6bec177eaec5cd
gilkrich <gildebil2@gmail.com>
2023-07-30 10:47:14 +0300

    man

--- controllers/clothescontrollers.js
+++ controllers/clothescontrollers.js
@@ -3,12 +3,30 @@ const Jeans = require('../models/jeans');
 const Pants = require('../models/pants');
 const Shirts = require('../models/womenshirts');
 const Top = require('../models/womentops');
-
+const Manshirts = require('../models/manshirts')
+const Manshorts = require('../models/manshorts')
+const Manpants = require('../models/manpants')
+const Mantshirts = require('../models/mantshirt')
 
 exports.addclothes = (req, res) => {
     let {productarray} = req.body
     try{
         let temp =  [Dress,Top,Shirts,Pants,Jeans]
+        for (let i = 0; i < productarray.length; i++) {
+            for (let index = 0; index < productarray[i].length; index++) {
+                temp[i].create({productname:productarray[i][index].productTitle,productimage:productarray[i][index].productImage,productfit:productarray[i][index].productFit,producturl:productarray[i][index].productLink,companyname:productarray[i][index].companyName})
+            }        
+        }
+        res.status(200).json('added')
+    }catch(err){
+        res.status(500).send(err)
+    }
+}
+
+exports.addmanclothes = (req, res) => {
+    let {productarray} = req.body
+    try{
+        let temp =  [Mantshirts,Manpants,Manshorts,Manshirts]
         for (let i = 0; i < productarray.length; i++) {
             for (let index = 0; index < productarray[i].length; index++) {
                 temp[i].create({productname:productarray[i][index].productTitle,productimage:productarray[i][index].productImage,productfit:productarray[i][index].productFit,producturl:productarray[i][index].productLink,companyname:productarray[i][index].companyName})
